4bef3efc62d7192c3328728ddec8c14ba123a9f6
modipriyam <55067505+modipriyam@users.noreply.github.com>
2019-10-31 02:35:38 -0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,67 +1,111 @@
-import {Observable} from 'rxjs';
 
-let getJSON = function (url) {
-    return new Promise(function (resolve, reject) {
-        let xhr = new XMLHttpRequest();
-        xhr.open('GET', url, true);
-        xhr.responseType = 'json';
-        xhr.onload = function () {
-            let status = xhr.status;
-            if (status == 200) {
-                resolve(xhr.response);
-            } else {
-                reject(status);
-            }
-        };
-        xhr.send();
-    });
-};
-getJSON('https://api.worldtradingdata.com/api/v1/stock?symbol=SNAP,TWTR,VOD.L&api_token=demo').then(function (data) {
-
-    alert('Your Json result is:  ' + data);
-    const res = data.data;
-    let dat = res.length;
-    console.log("Data at 0 index is  : " + data);
-    populate(res);
-}, function (status) { //error detection....
-    alert('Something went wrong.');
-});
-
-
-const rankingBody = document.querySelector("#result > tbody");
-
-function populate(res) {
-    console.log("in pop");
-    console.log(res);
-
-    //clearing existing data
-    while (rankingBody.firstChild) {
-        rankingBody.removeChild(rankingBody.firstChild);
-
+// xhr request promise call for fetching data from API
+let request = obj => {
+  return new Promise((resolve, reject) => {
+    let xhr = new XMLHttpRequest();
+    xhr.responseType = "json";
+    xhr.open(obj.method || "GET", obj.url);
+    if (obj.headers) {
+      Object.keys(obj.headers).forEach(key => {
+        xhr.setRequestHeader(key, obj.headers[key]);
+      });
     }
-
-    //populate table
-    res.forEach((row) => {
-        console.log(row);
-        const r = document.createElement("tr");
-        let x;
-        for (x in row) {
-            const c = document.createElement("td");
-            console.log(row[x]);
-
-            c.textContent = row[x];
-            r.appendChild(c);
-
+    xhr.onload = () => {
+      if (xhr.status >= 200 && xhr.status < 300) {
+        resolve(xhr.response);
+      } else {
+        reject(xhr.statusText);
+      }
+    };
+    xhr.onerror = () => reject(xhr.statusText);
+    xhr.send(obj.body);
+  });
+};
+// RX js observer to get JSON data from the API server
+let observer = {
+  next: function(value) {
+    var inputStockSymbols = stockSymbolsClass[0].value;
+    console.log();
+    request({
+      url: `https://api.worldtradingdata.com/api/v1/stock?symbol=SNAP,TWTR,VOD.L&api_token=demo`
+    })
+      .then(response => {
+        console.log(response);
+        let stocktable = document.getElementsByClassName("stockListTable")[0];
+        if (typeof stocktable != "undefined" && stocktable != null) {
+          // if it exists, remove it.
+          container.removeChild(stocktable);
         }
-        rankingBody.appendChild(r);
-    });
+        let inputSymbolsArray = inputStockSymbols.split(",");
+        let filteredData = response.data.filter(f =>
+          inputSymbolsArray.includes(f.symbol)
+        );
+        GenerateTable(filteredData);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  },
+  // In reality, the error and complete functions below would never be
+  // called for a button click event, but this shows the basic structure
+  // of an Observable; generally implementing the three functions: next, error, and complete...
+  error: function(error) {
+    console.log(error);
+  },
+  complete: function() {
+    console.log("Completed");
+  }
+};
 
+// method to generate table from the json output
+function GenerateTable(stockDetailsArray) {
+  //Build an array containing Customer records.
+  //Create a HTML Table element.
+
+  let table = document.createElement("table");
+  table.setAttribute("class", "stockListTable");
+  table.classList.add("table-responsive");
+  table.border = "1";
+
+  //Get the count of columns.
+
+  //Add the header row.
+  let row = table.insertRow(-1);
+  //var headerCount = 0;
+  for (let key in stockDetailsArray[0]) {
+    //headerCount++;
+    var headerCell = document.createElement("th");
+    headerCell.innerHTML = key;
+    row.appendChild(headerCell);
+  }
+
+  //Add the data rows.
+  for (let i = 0; i < stockDetailsArray.length; i++) {
+    row = table.insertRow(-1);
+    for (let key in stockDetailsArray[i]) {
+      let cell = row.insertCell(-1);
+      cell.innerHTML = stockDetailsArray[i][key];
+    }
+  }
+  container.appendChild(table);
+}
+function unsubscribeSubmitbtn() {
+  console.log("unload");
+  // Later:
+  // This cancels the ongoing Observable execution which
+  // was started by calling subscribe with an Observer.
+  subscriberObject.unsubscribe();
 }
 
-let button = document.querySelector('#submitbutton');
-let clicks$ = Rx.Observable
-    .fromEvent(button, 'click');
+let submitBtn = document.getElementsByClassName("submitBtn");
+let stockSymbolsClass = document.getElementsByClassName("stockSymbols");
+let container = document.getElementsByClassName("container")[0];
 
-clicks$.getJSON(e => {
-    console.log('increase count');
-});
+
+// event listener observer for click submit button using Rxjs
+let subscriberObject = Rx.Observable.fromEvent(submitBtn, "click").subscribe(
+  observer
+);
+
+
+window.onunload = unsubscribeSubmitbtn;
